880ecab052a158ad6b1f8e82b86fae671dcc2499
solomonade1 <solomonadegboyega1@gmail.com>
2023-11-07 13:14:30 +0100

    fix: cookie bug updatwes

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -43,7 +43,6 @@ export const login = async (req, res, next) => {
           res
             .cookie("access_token", token, {
               httpOnly: true,
-              origin: "http://localhost:3000",
             })
             .status(200)
             .json(otherDetails);
